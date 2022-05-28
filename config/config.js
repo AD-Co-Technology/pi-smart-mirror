b4e682d25edfae575356818a23fb25f8482f628a
thedigitalchief <me@dylanhnguyen.com>
2022-05-27 22:56:40 -0700

    added electron packages

--- config/config.js
+++ config/config.js
@@ -105,27 +105,6 @@ let config = {
 				broadcastNewsUpdates: true
 			}
 		},
-		{
-    module: "MMM-WeeklySchedule",
-    position: "top_left",
-    header: "Andrew's Daily Schedule",
-    config: {
-        schedule: {
-            timeslots: [ "8:00am", "8:30am", "9:00am","9:30am", "10:00am","10:30am", "11:00am","11:30am", "12:00pm","12:30pm", "1:00pm","1:30pm", "2:00pm","2:30pm", "3:00pm","3:30pm", "4:00pm","4:30pm", "5:00pm","5:30pm", "6:00pm","6:30pm", "7:00pm","7:30pm", "8:00pm"],
-            lessons: {
-                mon: [ "ECON 123: American Economic History", "8:30am", "9:00am","BUS 103: Marketing and Distribution Management Lecture", "10:00am","10:30am", "11:00am","11:30am", "BUS 103: Marketing and Distribution Management Discussion","12:30pm", "1:00pm","1:30pm", "2:00pm","2:30pm", "3:00pm","3:30pm", "4:00pm","4:30pm", "5:00pm","5:30pm", "6:00pm","6:30pm", "7:00pm","7:30pm", "8:00pm" ],  
-                tue: [ "8:00am", "8:30am", "9:00am","9:30am", "10:00am","10:30am", "11:00am","11:30am", "12:00pm","12:30pm", "1:00pm","1:30pm", "2:00pm","2:30pm", "3:00pm","3:30pm", "4:00pm","4:30pm", "ECON 119: Law and Economics","5:30pm", "6:00pm","6:30pm", "7:00pm","7:30pm", "8:00pm"],
-                wed: [ "ECON 123: American Economic History", "8:30am", "9:00am","BUS 103: Marketing and Distribution Management Lecture", "10:00am","10:30am", "11:00am","11:30am", "12:00pm","12:30pm", "1:00pm","1:30pm", "2:00pm","2:30pm", "3:00pm","3:30pm", "4:00pm","4:30pm", "5:00pm","5:30pm", "6:00pm","6:30pm", "7:00pm","7:30pm", "8:00pm" ],
-                thu: [ "8:00am", "8:30am", "9:00am","9:30am", "10:00am","10:30am", "11:00am","11:30am", "12:00pm","12:30pm", "1:00pm","1:30pm", "2:00pm","2:30pm", "3:00pm","3:30pm", "4:00pm","4:30pm", "ECON 119: Law and Economics","5:30pm", "6:00pm","BUS 138 International Finance", "7:00pm","7:30pm", "8:00pm" ],
-                fri: [ "8:00am", "8:30am", "9:00am","9:30am", "10:00am","10:30am", "11:00am","11:30am", "12:00pm","12:30pm", "1:00pm","1:30pm", "2:00pm","2:30pm", "3:00pm","3:30pm", "4:00pm","4:30pm", "5:00pm","5:30pm", "6:00pm","6:30pm", "7:00pm","7:30pm", "8:00pm" ],
-                
-                sun: [ "", "", "" ]   
-            }
-        },
-        updateInterval: 1 * 60 * 60 * 1000, 
-        showNextDayAfter: "16:00"
-    }
-},
 	]
 };
 
